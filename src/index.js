ae5b86923a438fc36d70cbdb0b7b5b6fa92b3fee
Amandar8 <amandaruth08@gmail.com>
2018-10-04 11:08:55 -0500

    fixed scroll to top react-router issue

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import './index.css';
 import App from './app/layout/App';
 import * as serviceWorker from './serviceWorker';
 import { configureStore } from './app/store/configureStore';
+import ScrollToTop from './app/common/util/ScrollToTop';
 
 const store= configureStore();
 
@@ -15,8 +16,10 @@ const rootEl = document.getElementById('root');
 let render = () => {
   ReactDOM.render(
     <Provider store={store}>
-    <BrowserRouter>
-      <App />
+      <BrowserRouter>
+        <ScrollToTop>
+        <App />
+        </ScrollToTop>  
     </BrowserRouter>
     </Provider>,
    rootEl
