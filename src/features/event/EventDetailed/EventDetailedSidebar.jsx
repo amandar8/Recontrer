00a83627bfbf879d88c81104dc6992e4d6a324b8
Amandar8 <amandaruth08@gmail.com>
2018-10-04 10:38:34 -0500

    added attendees to event

--- src/features/event/EventDetailed/EventDetailedSidebar.jsx
+++ src/features/event/EventDetailed/EventDetailedSidebar.jsx
@@ -1,40 +1,44 @@
-import React from 'react';
-import { Segment, List, Label, Item } from 'semantic-ui-react';
+import React from "react";
+import { Segment, List, Label, Item } from "semantic-ui-react";
 
-const EventDetailedSidebar = () => {
+const EventDetailedSidebar = ({ attendees }) => {
+  const isHost = false;
   return (
-       <div>
-          <Segment
-            textAlign="center"
-            style={{ border: 'none' }}
-            attached="top"
-            secondary
-            inverted
-            color="teal"
-          >
-            2 People Going
-          </Segment>
-          <Segment attached>
-            <List relaxed divided>
-              <Item style={{ position: 'relative' }}>
+    <div>
+      <Segment
+        textAlign="center"
+        style={{ border: "none" }}
+        attached="top"
+        secondary
+        inverted
+        color="teal"
+      >
+        2 People Going
+      </Segment>
+      <Segment attached>
+        <List relaxed divided>
+          {attendees &&
+            attendees.map(attendee => (
+              <Item key={attendee.is} style={{ position: "relative" }}>
                 <Label
-                  style={{ position: 'absolute' }}
+                  style={{ position: "absolute" }}
                   color="orange"
                   ribbon="right"
                 >
                   Host
                 </Label>
-                <Item.Image size="tiny" src="/assets/user.png" />
+                <Item.Image size="tiny" src={attendee.photoURL} />
                 <Item.Content verticalAlign="middle">
                   <Item.Header as="h3">
-                    <a>Attendee Name</a>
+                    <a>{attendee.name}</a>
                   </Item.Header>
                 </Item.Content>
               </Item>
-            </List>
-          </Segment>
-        </div>
-  )
-}
+            ))}
+        </List>
+      </Segment>
+    </div>
+  );
+};
 
-export default EventDetailedSidebar; 
+export default EventDetailedSidebar;
