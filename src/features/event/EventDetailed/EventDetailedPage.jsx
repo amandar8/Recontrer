2adb9f4987e247731eef595d721e08e51c756f3d
Amandar8 <amandaruth08@gmail.com>
2018-10-04 10:52:17 -0500

    able to view event

--- src/features/event/EventDetailed/EventDetailedPage.jsx
+++ src/features/event/EventDetailed/EventDetailedPage.jsx
@@ -1,40 +1,26 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Grid } from 'semantic-ui-react';
 import EventDetailedHeader from './EventDetailedHeader';
 import EventDetailedInfo from './EventDetailedInfo';
 import EventDetailedChat from './EventDetailedChat';
 import EventDetailedSidebar from './EventDetailedSidebar';
 
-const event = {
-  id: '1',
-  title: 'Trip to Texas State Capitol',
-  date: '2018-08-27',
-  category: 'culture',
-  description:
-    `Orient yourself to the Capitol Complex through exhibits, film, 
-    and information at the Visitors Center. Participate in a free 
-    guided tour of the Capitol. Discover the natural beauty of the 
-    Capitol Grounds. Buy a souvenir at the Gift Shop in the Capitol 
-    Extension or the Visitors Center.`,
-  city: 'Austin, TX',
-  venue: "Texas State Capitol Visitors Center",
-  hostedBy: 'Bob',
-  hostPhotoURL: 'https://randomuser.me/api/portraits/men/20.jpg',
-  attendees: [
-    {
-      id: 'a',
-      name: 'Bob',
-      photoURL: 'https://randomuser.me/api/portraits/men/20.jpg'
-    },
-    {
-      id: 'b',
-      name: 'Tom',
-      photoURL: 'https://randomuser.me/api/portraits/men/22.jpg'
-    }
-  ]
+const mapState = (state, ownProps) => {
+  const eventId = ownProps.match.params.id;
+
+  let event = {};
+
+  if (eventId && state.events.length > 0) {
+    event = state.events.filter(event => event.id === eventId)[0]
+  }
+
+  return {
+    event
+  }
 }
 
-const EventDetailedPage = () => {
+const EventDetailedPage = ({event}) => {
   return (
     <Grid>
       <Grid.Column width={10}>
@@ -49,4 +35,4 @@ const EventDetailedPage = () => {
   )
 }
 
-export default EventDetailedPage;
+export default connect(mapState)(EventDetailedPage);
