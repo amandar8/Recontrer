d68255d545e1982d624baefeb402045e489d7909
Amandar8 <amandaruth08@gmail.com>
2018-10-05 09:33:01 -0500

    working on form submission

--- src/app/common/form/PlaceInput.jsx
+++ src/app/common/form/PlaceInput.jsx
@@ -3,6 +3,12 @@ import { Form, Label } from 'semantic-ui-react';
 import Script from 'react-load-script';
 import PlacesAutoComplete from 'react-places-autocomplete';
 
+const styles = {
+  autocompleteContainer: {
+    zIndex:1000
+  }
+}
+
 class PlaceInput extends Component {
   state = {
     scriptLoaded: false
@@ -23,6 +29,7 @@ class PlaceInput extends Component {
           inputProps={{...input, placeholder}}
           options={options}
           onSelect={onSelect}
+          styles={styles}
         />}
         {touched && error && <Label basic color='red'>{error}</Label>}
       </Form.Field>
--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -1,7 +1,10 @@
+/*global google*/
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { reduxForm, Field } from "redux-form";
 import moment from 'moment';
+import { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
+import Script from 'react-load-script'
 import { composeValidators, combineValidators, isRequired, hasLengthGreaterThan } from 'revalidate';
 import cuid from "cuid";
 import { Segment, Form, Button, Grid, Header } from "semantic-ui-react";
@@ -53,6 +56,26 @@ const validate = combineValidators({
 })
 
 class EventForm extends Component {
+  state = {
+    cityLatLng: {},
+    venueLatLng: {},
+    scriptLoaded: false
+  }
+
+  handleScriptLoaded = () => this.setState({scriptLoaded: true});
+
+  handleCitySelect = (selectedCity) => {
+    geocodeByAddress(selectedCity)
+    .then(results => getLatLng(results[0]))
+    .then(latlng => {
+      this.setState({
+        cityLatLng: latlng
+      })
+    })
+    .then(() => {
+      this.props.change('city', selectedCity)
+    })
+  }
 
   onFormSubmit = values => {
     values.date = moment(values.date).format()
@@ -75,6 +98,10 @@ class EventForm extends Component {
     const {invalid, submitting, pristine} = this.props;
     return (
       <Grid>
+        <Script
+          url='https://maps.googleapis.com/maps/api/js?key=AIzaSyBpaNX8Y2bSPM5HbfeMon-ozKN_RWYjyE4&libraries=places'
+          onLoad={this.handleScriptLoaded}
+        />
         <Grid.Column width={10}>
           <Segment>
             <Header sub color="teal" content="Event Details" />
@@ -106,14 +133,21 @@ class EventForm extends Component {
                 type="text"
                 component={PlaceInput}
                 options={{types: ['(cities)']}}
-                placeholder="Event city"
+                placeholder="Event City"
+                onSelect={this.handleCitySelect}
               />
+              {this.state.scriptLoaded &&
               <Field
                 name="venue"
                 type="text"
-                component={TextInput}
-                placeholder="Event venue"
-              />
+                options={{
+                  location: new google.maps.LatLng(this.state.cityLatLng),
+                  radius: 1000,
+                  types: ['establishment']
+              }}
+                component={PlaceInput}
+                placeholder="Event Venue"
+              />}
               <Field
                 name="date"
                 type="text"
@@ -121,7 +155,7 @@ class EventForm extends Component {
                 dateFormat="YYYY-MM-DD HH:mm"
                 timeFormat='HH:mm'
                 showTimeSelect
-                placeholder="Date and time of event"
+                placeholder="Date and Time of event"
               />
               <Button disabled={invalid || submitting || pristine} positive type="submit">
                 Submit
