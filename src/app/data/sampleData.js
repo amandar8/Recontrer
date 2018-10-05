2a605dfa179d5cfeb453134e798ec804573a654b
Amandar8 <amandaruth08@gmail.com>
2018-10-05 14:49:46 -0500

    added event activity

--- src/app/data/sampleData.js
+++ src/app/data/sampleData.js
@@ -2,7 +2,7 @@ const sampleData = {
   events: [{
       id: '1',
       title: 'Trip to Texas State Capitol',
-      date: '2018-10-21',
+      date: '2018-10-21T18:00:00',
       category: 'culture',
       description: `Orient yourself to the Capitol Complex through exhibits, film, and information at the Visitors Center. Participate in a free guided tour of the Capitol. Discover the natural beauty of the Capitol Grounds. Buy a souvenir at the Gift Shop in the Capitol Extension or the Visitors Center.`,
       city: 'Austin',
@@ -28,7 +28,7 @@ const sampleData = {
     {
       id: '2',
       title: 'Austin Beer Trail',
-      date: '2018-11-18',
+      date: '2018-11-18T14:00:00',
       category: 'drinks',
       description: `Austin's craft brewing scene has been booming for the past few years, so you'd be forgiven for feeling a little overwhelmed by all of our fermented offerings. Whether you're in the mood for a Mexican-style lager to compliment your tour of Austin tacos and queso, looking to expand your palate with a sour-mashed saison or trying to keep cool with a refreshing IPA on a hot summer's day, we have you covered. Here is your 2-day insider’s itinerary to Austin’s craft beer trail.`,
       city: 'Austin',
