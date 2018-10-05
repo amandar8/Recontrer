ed3c4765d429b8cd6566f6b90d460dce6323ac21
Amandar8 <amandaruth08@gmail.com>
2018-10-05 10:52:49 -0500

    added google map to events

--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -75,10 +75,24 @@ class EventForm extends Component {
     .then(() => {
       this.props.change('city', selectedCity)
     })
+  };
+
+    handleVenueSelect = (selectedVenue) => {
+    geocodeByAddress(selectedVenue)
+    .then(results => getLatLng(results[0]))
+    .then(latlng => {
+      this.setState({
+        venueLatLng: latlng
+      })
+    })
+    .then(() => {
+      this.props.change('venue', selectedVenue)
+    })
   }
 
   onFormSubmit = values => {
-    values.date = moment(values.date).format()
+    values.date = moment(values.date).format();
+    values.venueLatLng = this.state.venueLatLng;
     if (this.props.initialValues.id) {
       this.props.updateEvent(values);
       this.props.history.goBack();
@@ -144,9 +158,10 @@ class EventForm extends Component {
                   location: new google.maps.LatLng(this.state.cityLatLng),
                   radius: 1000,
                   types: ['establishment']
-              }}
+                }}
                 component={PlaceInput}
                 placeholder="Event Venue"
+                onSelect={this.handleVenueSelect}
               />}
               <Field
                 name="date"
--- src/features/event/eventReducer.jsx
+++ src/features/event/eventReducer.jsx
@@ -1,66 +1,62 @@
 import { createReducer } from '../../app/common/util/reducerUtil';
 import { CREATE_EVENT, DELETE_EVENT, UPDATE_EVENT } from './eventConstants';
 
-const initialState = [
-  {
-    id: '1',
-    title: 'Trip to Texas State Capitol',
-    date: '2018-08-27',
-    category: 'culture',
-    description:
-      `Orient yourself to the Capitol Complex through exhibits, film, 
-      and information at the Visitors Center. Participate in a free 
-      guided tour of the Capitol. Discover the natural beauty of the 
-      Capitol Grounds. Buy a souvenir at the Gift Shop in the Capitol 
-      Extension or the Visitors Center.`,
-    city: 'Austin, TX',
-    venue: "Texas State Capitol Visitors Center",
-    hostedBy: 'Bob',
-    hostPhotoURL: 'https://randomuser.me/api/portraits/men/20.jpg',
-    attendees: [
-      {
-        id: 'a',
-        name: 'Bob',
-        photoURL: 'https://randomuser.me/api/portraits/men/20.jpg'
+ const initialState = [
+    {
+      id: '1',
+      title: 'Trip to Texas State Capitol',
+      date: '2018-10-21',
+      category: 'culture',
+      description: `Orient yourself to the Capitol Complex through exhibits, film, and information at the Visitors Center. Participate in a free guided tour of the Capitol. Discover the natural beauty of the Capitol Grounds. Buy a souvenir at the Gift Shop in the Capitol Extension or the Visitors Center.`,
+      city: 'Austin',
+      venue: 'Texas State Capitol Visitors Center, Austin, TX',
+      venueLatLng: {
+        lat: 30.274655,
+        lng: -97.740358
       },
-      {
-        id: 'b',
-        name: 'Tom',
-        photoURL: 'https://randomuser.me/api/portraits/men/22.jpg'
-      }
-    ]
-  },
-  {
-    id: '2',
-    title: 'Austin Beer Trail',
-    date: '2018-08-28',
-    category: 'drinks',
-    description:
-      `Austin's craft brewing scene has been booming for the past few years, 
-      so you'd be forgiven for feeling a little overwhelmed by all of our 
-      fermented offerings. Whether you're in the mood for a Mexican-style 
-      lager to compliment your tour of Austin tacos and queso, looking to 
-      expand your palate with a sour-mashed saison or trying to keep cool 
-      with a refreshing IPA on a hot summer's day, we have you covered. 
-      Here is your 2-day insider’s itinerary to Austin’s craft beer trail.`,
-    city: 'Austin, TX',
-    venue: 'Austin Beer Garden Brewing Co, Lazarus Brewing Company, Black Star Co-op',
-    hostedBy: 'Tom',
-    hostPhotoURL: 'https://randomuser.me/api/portraits/men/22.jpg',
-    attendees: [
-      {
-        id: 'b',
-        name: 'Tom',
-        photoURL: 'https://randomuser.me/api/portraits/men/22.jpg'
+      hostedBy: 'Bob',
+      hostPhotoURL: 'https://randomuser.me/api/portraits/men/20.jpg',
+      attendees: [
+        {
+          id: 'a',
+          name: 'Bob',
+          photoURL: 'https://randomuser.me/api/portraits/men/20.jpg'
+        },
+        {
+          id: 'b',
+          name: 'Tom',
+          photoURL: 'https://randomuser.me/api/portraits/men/22.jpg'
+        }
+      ]
+    },
+    {
+      id: '2',
+      title: 'Austin Beer Trail',
+      date: '2018-11-18',
+      category: 'drinks',
+      description: `Austin's craft brewing scene has been booming for the past few years, so you'd be forgiven for feeling a little overwhelmed by all of our fermented offerings. Whether you're in the mood for a Mexican-style lager to compliment your tour of Austin tacos and queso, looking to expand your palate with a sour-mashed saison or trying to keep cool with a refreshing IPA on a hot summer's day, we have you covered. Here is your 2-day insider’s itinerary to Austin’s craft beer trail.`,
+      city: 'Austin',
+      venue: 'Austin Beer Garden Brewing Co, Lazarus Brewing Company, Black Star Co-op, Austin, TX',
+      venueLatLng: {
+        lat: 530.245310,
+        lng: -97.768930
       },
-      {
-        id: 'a',
-        name: 'Bob',
-        photoURL: 'https://randomuser.me/api/portraits/men/20.jpg'
-      }
-    ]
-  }
-]
+      hostedBy: 'Tom',
+      hostPhotoURL: 'https://randomuser.me/api/portraits/men/22.jpg',
+      attendees: [
+        {
+          id: 'a',
+          name: 'Bob',
+          photoURL: 'https://randomuser.me/api/portraits/men/20.jpg'
+        },
+        {
+          id: 'b',
+          name: 'Tom',
+          photoURL: 'https://randomuser.me/api/portraits/men/22.jpg'
+        }
+      ]
+    }
+  ];
 
 export const createEvent = (state, payload) => {
   return [...state, Object.assign({}, payload.event)]
